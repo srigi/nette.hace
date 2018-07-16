5095019d44b7e2a4f278b29ce4ea71f7aeefe743
srigi <srigi@srigi.sk>
2018-07-16 16:39:05 +0200

    Add listing of the Persons on the homepage

--- src/presenters/HomepagePresenter.php
+++ src/presenters/HomepagePresenter.php
@@ -2,7 +2,26 @@
 
 namespace App\Presenters;
 
+use Nette\Database;
+
 
 class HomepagePresenter extends WebPresenter
 {
+
+    /** @var Database\Connection */
+    private $database;
+
+
+    public function __construct(Database\Connection $database)
+    {
+        $this->database = $database;
+    }
+
+
+    public function renderDefault()
+    {
+        $persons = $this->database->query('SELECT * FROM person ORDER BY created_time DESC');
+        $this->template->persons = $persons;
+    }
+
 }
