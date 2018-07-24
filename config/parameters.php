92950badba303146cb2fc77bb678762efb5aab26
srigi <srigi@srigi.sk>
2018-07-24 15:07:14 +0200

    Remove useless ENV vars & neon params

--- config/parameters.php
+++ config/parameters.php
@@ -4,11 +4,4 @@ declare(strict_types = 1);
 
 $parameters = [];
 
-if (($appHostProto = \getenv('APP_HOST_PROTO')) !== false) {
-    $parameters['appHostProto'] = $appHostProto;
-}
-if (($appHostUri = \getenv('APP_HOST_URI')) !== false) {
-    $parameters['appHostUri'] = $appHostUri;
-}
-
 return ['parameters' => $parameters];
